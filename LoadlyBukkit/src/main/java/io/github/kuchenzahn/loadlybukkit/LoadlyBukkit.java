54fa2d0323d36b3d1b373121ee6fe876f81bed2a
Kuch2nZahn <webcode_mail@yahoo.com>
2024-05-23 01:29:04 +0200

    Added Message & Confighandler and Server Startup

--- LoadlyBukkit/src/main/java/io/github/kuchenzahn/loadlybukkit/LoadlyBukkit.java
+++ LoadlyBukkit/src/main/java/io/github/kuchenzahn/loadlybukkit/LoadlyBukkit.java
@@ -1,17 +1,81 @@
 package io.github.kuchenzahn.loadlybukkit;
 
+import io.github.kuchenzahn.LoadlyServer;
+import io.github.kuchenzahn.LoadlyUUID;
+import io.github.kuchenzahn.event.EventRegistry;
+import io.github.kuchenzahn.loadlybukkit.config.ConfigManager;
+import io.github.kuchenzahn.loadlybukkit.message.MessageHandler;
+import io.github.kuchenzahn.registry.LoadlyPacketRegistry;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.net.InetSocketAddress;
+
 public final class LoadlyBukkit extends JavaPlugin {
+    private static LoadlyBukkit instance;
+
+    private ConfigManager configManager;
+    private MessageHandler messageHandler;
+
+    private LoadlyServer loadlyServer;
+    private EventRegistry eventRegistry;
+    private LoadlyPacketRegistry packetRegistry;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        this.configManager = new ConfigManager();
+        configManager.checkConfig();
+
+        this.messageHandler = new MessageHandler();
+
+        startLoadlyServer();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        loadlyServer.shutdown();
+    }
+
+    private void startLoadlyServer() {
+        FileConfiguration config = getConfig();
+        String host = config.contains("host") ? config.getString("host") : "localhost";
+        int port = config.contains("port") ? config.getInt("port") : 2244;
+
+        InetSocketAddress address = new InetSocketAddress(host, port);
+
+        MessageHandler.sendConsoleMessage("Starting LoadlyServer on " + address.toString(), MessageHandler.MessageType.INFO);
+
+        LoadlyUUID loadlyServerUUID = LoadlyUUID.LoadlyUUIDS.SERVER.get();
+        this.loadlyServer = new LoadlyServer(address, packetRegistry, future -> {}, loadlyServerUUID);
+
+        MessageHandler.sendConsoleMessage("LoadlyServer started with Server ID: " + loadlyServerUUID.getUuid().toString(), MessageHandler.MessageType.INFO);
+        MessageHandler.sendConsoleMessage("LoadlyServer ID 00000000-0000-0000-0000-000000000000 is reserved for the server itself, this is not an ERROR!", MessageHandler.MessageType.WARNING);
+    }
+
+    public LoadlyServer getLoadlyServer() {
+        return loadlyServer;
+    }
+
+    public EventRegistry getEventRegistry() {
+        return eventRegistry;
+    }
+
+    public LoadlyPacketRegistry getPacketRegistry() {
+        return packetRegistry;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public MessageHandler getMessageHandler() {
+        return messageHandler;
+    }
+
+    public static LoadlyBukkit getInstance() {
+        return instance;
     }
 }
