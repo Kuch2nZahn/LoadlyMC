7f06a8a7ccda9fba2c8c7f9ce8d397cde01eed24
Kuch2nZahn <webcode_mail@yahoo.com>
2024-05-22 23:51:40 +0200

    Finished Loadly Net

---
+++ LoadlyBukkit/src/main/java/io/github/kuchenzahn/loadlybukkit/LoadlyBukkit.java
@@ -0,0 +1,17 @@
+package io.github.kuchenzahn.loadlybukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LoadlyBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
