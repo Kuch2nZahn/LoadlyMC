896c2e3a2a286efb8ed66ebd0724523dae848f9e
Kuch2nZahn <webcode_mail@yahoo.com>
2024-05-23 17:48:35 +0200

    Removed Message Content

--- LoadlyNet/src/main/java/io/github/kuchenzahn/packet/impl/LoadlyHelloC2SPacket.java
+++ LoadlyNet/src/main/java/io/github/kuchenzahn/packet/impl/LoadlyHelloC2SPacket.java
@@ -4,27 +4,17 @@ import io.github.kuchenzahn.buffer.PacketBuffer;
 import io.github.kuchenzahn.packet.LoadlyPacket;
 
 public class LoadlyHelloC2SPacket extends LoadlyPacket {
-    private String message;
 
     public LoadlyHelloC2SPacket(){
 
     }
 
-    public LoadlyHelloC2SPacket(String message){
-        this.message = message;
-    }
-
     @Override
     public void read(PacketBuffer buffer) {
-        this.message = buffer.readUTF8();
     }
 
     @Override
     public void write(PacketBuffer buffer) {
-        buffer.writeUTF8(message);
     }
 
-    public String getMessage() {
-        return message;
-    }
 }
