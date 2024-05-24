b1ba76bbadc3b7c9e552fa1704d4ceb19e3e42ff
Kuch2nZahn <webcode_mail@yahoo.com>
2024-05-24 16:54:44 +0200

    Added LoadlyhelloS2CPacket

---
+++ LoadlyNet/src/main/java/io/github/kuchenzahn/packet/impl/LoadlyHelloS2CPacket.java
@@ -0,0 +1,20 @@
+package io.github.kuchenzahn.packet.impl;
+
+import io.github.kuchenzahn.buffer.PacketBuffer;
+import io.github.kuchenzahn.packet.LoadlyPacket;
+
+public class LoadlyHelloS2CPacket extends LoadlyPacket {
+    public LoadlyHelloS2CPacket(){
+
+    }
+
+    @Override
+    public void read(PacketBuffer buffer) {
+
+    }
+
+    @Override
+    public void write(PacketBuffer buffer) {
+
+    }
+}
--- LoadlyNet/src/main/java/io/github/kuchenzahn/registry/LoadlyPacketRegistry.java
+++ LoadlyNet/src/main/java/io/github/kuchenzahn/registry/LoadlyPacketRegistry.java
@@ -2,6 +2,7 @@ package io.github.kuchenzahn.registry;
 
 import io.github.kuchenzahn.exception.PacketRegistrationException;
 import io.github.kuchenzahn.packet.impl.LoadlyHelloC2SPacket;
+import io.github.kuchenzahn.packet.impl.LoadlyHelloS2CPacket;
 
 public class LoadlyPacketRegistry extends SimplePacketRegistry{
     public LoadlyPacketRegistry(){
@@ -9,6 +10,7 @@ public class LoadlyPacketRegistry extends SimplePacketRegistry{
 
         try {
             registerPacket(1, LoadlyHelloC2SPacket.class);
+            registerPacket(2, LoadlyHelloS2CPacket.class);
         } catch (PacketRegistrationException exception){
             exception.printStackTrace();
         }
